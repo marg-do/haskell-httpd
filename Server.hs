2a041abf9da8bff498acbb6da0d3f9875347a839
marg_do <t.t.mc1192.sf@gmail.com>
2014-05-07 02:45:48 +0900

    make request parser

--- Server.hs
+++ Server.hs
@@ -1,21 +1,29 @@
 import System.IO
+import Data.List
 import Data.List.Split
 import Network
 import Control.Exception
 import Control.Concurrent
 
 type Log = String
+type Request = String
 type RequestLine = String
 
+type Env = (String, String)
+
+--------------------------------------------------
+-- Configuration
 portNumber :: PortNumber
 portNumber = 8080
 
 rootDir :: FilePath
 rootDir = "/home/marg_do/work/haskell/Server/www"
 
+-- Logger
 logStartServer :: PortNumber -> Log
 logStartServer port = "server has started, listening on: " ++ show port
 
+-- Request Parser(Old)
 parseToPath :: RequestLine -> String
 parseToPath reql = splitOn " " reql !! 1
 
@@ -27,7 +35,20 @@ isRequestGet reql = if method == "GET" then True
                     else False
   where method = splitOn " " reql !! 0
 
+-- Request Parser
+reqToReql :: Request -> [RequestLine]
+reqToReql = lines
 
+reqlToEnv :: RequestLine -> Env
+reqlToEnv reql | any (== ':') reql = (splited_cln !! 0, splited_cln !! 1)
+               | otherwise = (splited_space !! 0, splited_space !! 1)
+  where splited_cln = splitOn ": " reql
+        splited_space = splitOn " " reql
+
+parseRequest :: Request -> [Env]        
+parseRequest req = map reqlToEnv $ reqToReql req
+
+--------------------------------------------------
 
 onError :: String -> SomeException ->  IO String
 onError mes e = do
